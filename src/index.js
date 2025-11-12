7f7a57f035d179b7eccf32385cdb5937d1fef541
Sahar El-behiry <sahar.mis2015@gmail.com>
2025-11-12 18:28:56 +0200

    tic tac toe basics

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import { StrictMode } from "react";
+// React’s library to talk to web browsers (React DOM)
+import { createRoot } from "react-dom/client";
+import "./styles.css";
+
+import App from "./App";
+
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
